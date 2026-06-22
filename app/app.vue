99e89db2e7c78fa67a4214f98014bcb23a062ec9
Javier Urbina Rathgeb <urbinarathgeb@gmail.com>
2026-06-22 17:41:40 -0400

    fix: eliminar NuxtRouteAnnouncer que mostraba texto 'Cargando...' en transiciones

--- app/app.vue
+++ app/app.vue
@@ -8,7 +8,6 @@
       :duration="2000"
       color="repeating-linear-gradient(90deg, var(--ui-color-primary-400), var(--ui-color-accent-400), var(--ui-color-primary-400))"
     />
-    <NuxtRouteAnnouncer />
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
